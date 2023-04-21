5bca36189f9b01ff613d4ceb8a44643a638a9084
r1sh11516 <rishikeshbhalekar6@gmail.com>
2023-04-21 10:28:02 +0000

    update in app.js and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { mockDogoData } from "./mockData";
 import "./styles.css";
 
 export default function App() {
   const [isDogoSelcted, setDogoSelected] = useState(false);
   const [dogoInfo, setDogoInfo] = useState([]);
+
+  const [isLightTheme, setLightTheme] = useState(true);
+  const [isFilter, setFilter] = useState(false);
+
+  const [filterBy, setFilterBy] = useState("");
+  const [filterList, setFilterList] = useState([]);
+
+  const [type, setType] = useState("");
+  const stringToCamleCase = (str) => {
+    let strToArr = str.split(" ");
+    let firstWord =
+      strToArr[0].charAt(0).toLowerCase() + strToArr[0].slice(1).toLowerCase();
+    let remWords = "";
+    let removedFirstInx = strToArr.splice(1, strToArr.length);
+    removedFirstInx.map((word) => {
+      return (remWords +=
+        word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+    });
+    return firstWord + remWords;
+  };
+
+  const countryList = [];
+  mockDogoData.map((el) =>
+    el.origin?.length > 1 || el.country_code?.length > 1
+      ? countryList.push(el.origin || el.country_code)
+      : ""
+  );
+
+  var countrySet = new Set(countryList);
+  // country list
+  var country = [...countrySet].join(" ").replaceAll(",", "").split(" ");
+
+  const breedGroupList = [];
+  mockDogoData.map((el) =>
+    el.breed_group?.length > 1 ? breedGroupList.push(el.breed_group) : ""
+  );
+  var breedDroupSet = new Set(breedGroupList);
+  // breed grp list
+  var breedGroup = [...breedDroupSet].join(" ").replaceAll(",", "").split(" ");
 
   const dogoDetails = (
     <div>
@@ -27,7 +66,7 @@ export default function App() {
             </tr>
             <tr>
               <th>Origin</th>
-              <td>{dogoInfo.origin}</td>
+              <td>{dogoInfo.origin || dogoInfo.country_code}</td>
             </tr>
             <tr>
               <th>Purpose</th>
@@ -51,25 +90,137 @@ export default function App() {
     </div>
   );
 
-  const dogoElement = mockDogoData.map((el, key) => (
+  const dogoElement = mockDogoData.map((el, key) => {
+    let dogoComponent = "";
+    if (el.breed_group === type) {
+      dogoComponent = (
+        <div
+          className={`dogo-name pet-${isLightTheme ? "light" : "dark"}-theme`}
+          key={key}
+          onClick={() => {
+            setDogoInfo(el);
+            setDogoSelected(true);
+          }}
+        >
+          <span>{el.name}</span>
+        </div>
+      );
+    }
+    if (el.origin?.includes(type) || el.country_code?.includes(type)) {
+      dogoComponent = (
+        <div
+          className={`dogo-name pet-${isLightTheme ? "light" : "dark"}-theme`}
+          key={key}
+          onClick={() => {
+            setDogoInfo(el);
+            setDogoSelected(true);
+          }}
+        >
+          <span>{el.name}</span>
+        </div>
+      );
+    }
+    if (type === "") {
+      dogoComponent = (
+        <div
+          className={`dogo-name pet-${isLightTheme ? "light" : "dark"}-theme`}
+          key={key}
+          onClick={() => {
+            setDogoInfo(el);
+            setDogoSelected(true);
+          }}
+        >
+          <span>{el.name}</span>
+        </div>
+      );
+    }
+
+    return dogoComponent;
+  });
+
+  const filterOnClickHandler = (grpName) => {
+    setFilterBy(stringToCamleCase(grpName));
+  };
+
+  useEffect(() => {
+    if (filterBy === "breedGroup") {
+      setFilterList(breedGroup);
+    } else if (filterBy === "country") {
+      setFilterList(country);
+    }
+  }, [filterBy, type]);
+
+  const filterComponent = (
     <div
-      className="dogo-name"
-      key={key}
-      onClick={() => {
-        setDogoInfo(el);
-        setDogoSelected(true);
-      }}
+      className={`${isDogoSelcted ? "nav-bar-none" : "filter-container"}  pet-${
+        isLightTheme ? "light" : "dark"
+      }-theme`}
     >
-      <span>{el.name}</span>
+      {["Country", "Breed Group"].map((grpName, key) => (
+        <span
+          key={key}
+          className={`group-element pet-${
+            isLightTheme ? "light" : "dark"
+          }-theme`}
+          onClick={() => filterOnClickHandler(grpName)}
+        >
+          {grpName}
+        </span>
+      ))}
     </div>
-  ));
+  );
+
+  const selectedFilter = (
+    <div
+      className={`${
+        filterList.length ? "filter-is-visible" : "filter-is-not-visible"
+      } ${
+        isDogoSelcted ? "nav-bar-none" : "selected-filter-list-container"
+      }  pet-${isLightTheme ? "light" : "dark"}-theme`}
+    >
+      {filterList?.map((ele) => (
+        <span
+          className={`filter-element pet-${
+            isLightTheme ? "light" : "dark"
+          }-theme`}
+          onClick={() => setType(ele)}
+        >
+          {ele}
+        </span>
+      ))}
+    </div>
+  );
+
   return (
-    <div className={`App ${isDogoSelcted ? "pop-up" : ""}`}>
+    <div
+      className={`App ${isDogoSelcted ? "pop-up" : ""} ${
+        isLightTheme ? "light" : "dark"
+      }-theme`}
+    >
       <div className={`nav-bar ${isDogoSelcted ? "nav-bar-none" : ""}`}>
         <span className="pet-title">Pet Lover</span>
-        <span className="pet-filter">Filter</span>
+        <span className="pet-control">
+          <span
+            className={`pet-filter pet-${
+              isLightTheme ? "light" : "dark"
+            }-theme`}
+            onClick={() => setFilter(!isFilter)}
+          >
+            Filter
+          </span>
+          <span
+            onClick={() => {
+              setLightTheme(!isLightTheme);
+            }}
+            className={`pet-${isLightTheme ? "light" : "dark"}-theme`}
+          >
+            {isLightTheme ? "Light-theme" : "Dark-theme"}
+          </span>
+        </span>
       </div>
-      <div className="body-container">
+      {isFilter ? filterComponent : ""}
+      {isFilter ? selectedFilter : ""}
+      <div className={`body-container`}>
         {isDogoSelcted ? dogoDetails : dogoElement}
       </div>
     </div>
